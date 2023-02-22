7cd415273385a8e048705bc5d92cbd40535655b5
Jordan-Conway <D00244954@student.dkit.ie>
2023-02-22 16:19:35 +0000

    SQL files now have their own folder

--- SQL/CreateTables.sql
+++ SQL/CreateTables.sql
@@ -4,9 +4,11 @@ CREATE TABLE `products` (
 `productId` INT(12) NOT NULL,
 `productName` VARCHAR(50) NOT NULL,
 `productPrice` DECIMAL(10, 2) NOT NULL,
-`manufacturer` VARCHAR(50) NOT NULL,
+`productCatagory` VARCHAR(50) NOT NULL,
 `noInStock` INT(10) NOT NULL,
 PRIMARY KEY(`productId`)
 );
 
-INSERT INTO `products` (`productId`, `productName`, `productPrice`, `manufacturer`, `noInStock`) VALUES
+INSERT INTO `products` (`productId`, `productName`, `productPrice`, `productCatagory`, `noInStock`) VALUES
+(1, "Number 7: Lucky Straight", 0.07, "Single", 13);
+();
