ddb934c87823959df0f3f99f957c893e434c2c61
Jordan-Conway <D00244954@student.dkit.ie>
2023-02-22 16:35:30 +0000

    Styled contact form

--- contact.php
+++ contact.php
@@ -25,31 +25,33 @@ label,a
 <?php include 'includes/header.php' ?>
 
 <body>
-<h1>Contact us</h1>
-<form method="POST" name="contactform" action="contact-form-handler.php"> 
+<div class="starter-template text-center">
+	<h1>Contact us</h1>
+</div>
+<form method="POST" name="contactform" action="contact-form-handler.php" class="mb-3"> 
 <p>
-<label for='name'>Your Name:</label> <br>
-<input type="text" name="name">
+<label for='name' class="form-label">Your Name:</label> <br>
+<input type="text" name="name" class="form-control"> <br>
 </p>
 <p>
-<label for='email'>Email Address:</label> <br>
-<input type="text" name="email"> <br>
+<label for='email' class="form-label">Email Address:</label> <br>
+<input type="text" name="email" class="form-control"> <br>
 </p>
 <p>
-<label for="phone">Phone</label> <br>
-<input type="tel" name="phone" placeholder="1234567890"> <br>
+<label for="phone" class="form-label">Phone</label> <br>
+<input type="tel" name="phone" placeholder="1234567890" class="form-control"> <br>
 </p>
 <p>
-<label for="date">Date</label> <br>
-<input type="date" name="date"> <br>
+<label for="date" class="form-label">Date</label> <br>
+<input type="date" name="date" class="form-control"> <br>
 </p>
 <p>
-<label for="reservationNumber">Reservation Number</label> <br>
-<input type="number" name="reservationNumber"> <br>
+<label for="reservationNumber" class="form-label">Reservation Number</label> <br>
+<input type="number" name="reservationNumber" class="form-control"> <br>
 </p>
 <p>
-<label for='message'>Message:</label> <br>
-<textarea name="message"></textarea>
+<label for='message' class="form-label">Message:</label> <br>
+<textarea name="message" class="form-control"></textarea>
 </p>
 <input type="submit" value="Submit"><br>
 </form>
