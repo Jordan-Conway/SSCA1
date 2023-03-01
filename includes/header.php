db82dc34c264be7d7c3245724c9cc1afe70ed7f4
Jordan-Conway <D00244954@student.dkit.ie>
2023-03-01 16:51:34 +0000

    Added link to home on nav bar

--- includes/header.php
+++ includes/header.php
@@ -10,6 +10,9 @@
           </ul>
           <span class="d-flex">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="index.php">Home</a>
+              </li>
               <li class="nav-item">
                 <a class="nav-link" aria-current="page" href="products.php">Products</a>
               </li>
