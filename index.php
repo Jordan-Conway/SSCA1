82de1dc135cf2ebb5a72b125adcb300d232d21fd
Jordan-Conway <D00244954@student.dkit.ie>
2023-03-01 16:46:54 +0000

    Added links on index page

--- index.php
+++ index.php
@@ -20,6 +20,20 @@
   <div class="starter-template text-center">
     <h1>Bootstrap starter template</h1>
     <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+    <div class="link-container">
+      <div class="link-tab">
+        <img class="link-image" src="images\products.jpg" alt="products"></img>
+        <a href="products.php">Products</a>
+      </div>
+      <div class="link-tab">
+        <img class="link-image" src="images\products.jpg" alt="products"></img>
+        <a href="page-2.php"></a>
+      </div>
+      <div class="link-tab">
+        <img class="link-image" src="images\products.jpg" alt="products"></img>
+        <a href="about.php">About</a>
+      </div>
+    </div>
   </div>
 
 </main><!-- /.container -->
