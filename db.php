28589f9096dc65607e66fcab141ee686c57348cb
Jordan-Conway <D00244954@student.dkit.ie>
2023-03-06 14:02:39 +0000

    DB now changes based on location

--- db.php
+++ db.php
@@ -1,7 +1,15 @@
 <?php
     $dsn = 'mysql:host=localhost;dbname=D00244954';
-    $username = 'D00244954';
-    $password = '2aslKKHV';
+    //This code should be removed for the final build
+    $host = $_SERVER['HTTP_HOST'];
+    if($host == "localhost"){
+        $username = 'root';
+        $password = '';
+    }
+    else{
+        $username = 'D00244954';
+        $password = '2aslKKHV';
+    }
 
     try {
         $db = new PDO($dsn, $username, $password);
