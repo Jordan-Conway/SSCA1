ef1385c371b581a5b08c179d3c4bf950c90b4762
Jordan-Conway <D00244954@student.dkit.ie>
2023-03-09 21:02:40 +0000

    Updated Titles

--- about.php
+++ about.php
@@ -6,7 +6,7 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.80.0">
-    <title>Starter Template · Bootstrap v5.0</title>
+    <title>About</title>
     <!-- Bootstrap core CSS -->
 <link href="css/bootstrap.min.css" rel="stylesheet">
     <!-- Custom styles for this template -->
